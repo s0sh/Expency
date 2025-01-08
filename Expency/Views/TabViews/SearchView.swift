1b178164933ad32bb9ea6d4c47b575fe9d68e445
Roman Bigun <proffcoders@gmail.com>
2025-01-09 01:44:38 +0200

    SearchView

--- Expency/Views/TabViews/SearchView.swift
+++ Expency/Views/TabViews/SearchView.swift
@@ -6,10 +6,42 @@
 //
 
 import SwiftUI
+import Combine
 
 struct SearchView: View {
+    
+    @State private var searchText: String = ""
+    @State private var filterText: String = ""
+    
+    var searchPublisher = PassthroughSubject<String, Never>()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            ScrollView(.vertical) {
+                LazyVStack(spacing: 12) {
+                    
+                }
+            }
+            .overlay(content: {
+                ContentUnavailableView("Search Transactions", systemImage: "magnifyingglass")
+                    .opacity(filterText.isEmpty ? 1: 0)
+            })
+            .onChange(of: searchText, { oldValue, newValue in
+                if newValue.isEmpty {
+                    searchText = ""
+                }
+                searchPublisher.send(newValue)
+            })
+            .onReceive(searchPublisher.debounce(for: .seconds(0.3),
+                                                scheduler: DispatchQueue.main),
+                       perform: { text in
+                filterText = text
+                print(text)
+            })
+            .searchable(text: $searchText)
+            .navigationTitle("Search")
+            .background(.gray.opacity(0.15))
+        }
     }
 }
 
